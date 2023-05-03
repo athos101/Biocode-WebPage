078bd101d7d9aa73aa3f2c762a2d92ccb4c957c7
Athos Schuck <outracoisa@email.com>
2023-05-02 23:54:04 -0300

    finalizando a seção serviços

--- src/components/services.vue
+++ src/components/services.vue
@@ -6,13 +6,15 @@
             <ServiceCard class="service-card" :title="mobileTitle" :text="mobileText" :item="mobileItem" />
             <ServiceCard class="service-card" :title="webdevTitle" :text="webdevText" :item="webdevItem" />
         </div>
-
+            
         <div class="row-2">
-
+            <ServiceCard class="service-card" :title="sistemasTitle" :text="sistemasText" :item="sistemasItem" />
+            <ServiceCard class="service-card" :title="datascienceTitle" :text="datascienceText" :item="datascienceItem" />
         </div>
 
         <div class="row-3">
-
+            <ServiceCard class="service-card" :title="educationTitle" :text="educationText" :item="educationItem" />
+            <ServiceCard class="service-card" :title="consultTitle" :text="consultText" :item="consultItem" />
         </div>
     </div>
 </template>
@@ -74,6 +76,15 @@ export default {
             sistemasTitle: "Sistemas de Informação",
             sistemasText: "Podemos te ajudar a manter seus dados mais organizados e seguros por meio de sistemas personalizados de acordo com as suas necessidades, podendo estes serem integrados aos sistemas já existentes.",
             sistemasItem: "sistema-operacional.png",
+            datascienceTitle: "Business Intelligence e Ciência de Dados",
+            datascienceText: "Tomar decisões baseadas em evidências é imprescindível na saúde. Utilizando tecnologias modernas para a coleta, transformação e carregamento de dados, construímos dashboards interativos para gerar informações de qualidade a partir da sua base de dados, potencializando os insights da equipe.",
+            datascienceItem: "data-science.png",
+            educationTitle: "Educação em Saúde",
+            educationText: "Construção de artefatos digitais que sirvam de apoio à educação em saúde, buscando o desenvolvimento de soluções que possam abranger aspectos de gamificação e realidade virtual.",
+            educationItem: "relatorio-de-saude.png",
+            consultTitle: "Consultoria em TI",
+            consultText: "O surgimento de novas tecnologias se tornou uma tendência nos últimos anos. Nossos serviços podem auxiliar no planejamento de projetos, na tomada de decisões e no acompanhamento de todas as atividades para que a sua empresa se mantenha competitiva e atuante no mercado.",
+            consultItem: "consultor.png",
         }
     }
 }
