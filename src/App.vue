f01796fcae1d8276a3283db3f4e9a66bc4929725
sarahlimajaeger <sarah.jaeger@ufcspa.edu.br>
2023-05-10 01:28:24 -0300

    História, Missão e Visão

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <!-- <Header /> -->
+    <header>
+      <div class="logo">Logo Here</div>
+      <nav class="header_nav">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a @click="scroll" style="cursor: pointer;">Footer</a></li>
+        </ul>
+      </nav>
+      <div class="cta">
+        <button>Sign Up</button>
+      </div>
+    </header>
+    <div id="body">
+      <Company />
+      <Company />
+      <Historia />
+      <Services />
+      <Contato />
+    </div>  
+
+      <section id="footer">
+        <Footer />
+      </section>
+      <router-view />
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
-export default {
+import Header from './components/header_exapmle.vue'
+import Footer from './components/footer_example.vue'
+import Company from './components/about_our_company.vue'
+import Services from './components/services.vue'
+import Contato from './components/contato.vue'
+import Historia from './components/historia.vue'
+
+export default{
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Header,
+    Company,
+    Footer,
+    Services,
+    Contato,
+    Historia,
+  },
+  methods: {
+    scroll() {
+      const element = document.getElementById('footer');
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  },
+
 }
 </script>
 
