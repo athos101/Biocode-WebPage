3c6aaf5f650400af72035183c710c3cdf1bfd152
unknown <yuridasilvaa1@gmail.com>
2023-05-12 12:38:23 -0300

    atualizando a members

--- src/components/members.vue
+++ src/components/members.vue
@@ -1,32 +1,35 @@
 <template>
   <h1 class="title">Membros</h1>
   <section class="container">
-    <div class="image-container">
-      <img
-        v-bind:src="images[currentImage]"
-        class="{ 'animate__animated': shouldUpdateImage, 'animate__fadeOut': shouldUpdateImage }"
-        @animationend="shouldUpdateImage = false"
-        alt="imagemMembro"
-        style="height: 480px;"
-        top="92"
-        left="437"
-      />
-    </div>
-    <div class="controls">
+    <div class="content-wrapper">
+      <div class="controls">
         <button @click="previousImage">
           <img src="../assets/arrow-left-solid.svg" alt="arrow-left" width="27" height="27">
-          </button>
+        </button>
+      </div>
+      <div class="image-container">
+        <transition name="fade" mode="out-in">
+          <img
+            :key="currentImage"
+            v-bind:src="images[currentImage]"
+            class="fade"
+            alt="imagemMembro"
+            style="height: 480px;"
+            top="92"
+            left="437"
+          />
+        </transition>
+      </div>
+      <div class="controls">
         <button @click="nextImage">
           <img src="../assets/arrow-right-solid.svg" alt="arrow-right" width="27" height="27">
         </button>
+      </div>
     </div>
-</section>
-    
+  </section>
 </template>
 
 <style scoped>
-
-@import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css");
 
 .container{
    background-color: rgba(217, 217, 217, 0.1);;
@@ -34,19 +37,38 @@
     height: 600px;
     width: 1000px;
     display: flex;
-    flex-direction: column;
     align-items: center;
     justify-content: center;
     margin:auto;
     margin-bottom: 120px;
 }
 
-.image-container {
+.content-wrapper {
   display: flex;
   align-items: center;
-  justify-content: center;
-  
+  justify-content: space-between;
+  width: 100%;
+}
+
+.image-container {
+   margin-right: 0 20px;
+   flex-flow: 1;
   }
+
+  #images {
+    opacity: 1;
+  }
+  
+  .fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
 
 
 .title {
@@ -60,10 +82,6 @@
 
 .controls{
     display: flex;
-    justify-content: space-between;
-    top: 0%;
-    width: 95%;
-    position: relative;
     filter: invert(100%);
 }
 
@@ -82,30 +100,26 @@ button {
 
 
 <script>
-export default{
-    name: 'Members',
-
-data() {
-
-    return{
-    images: [
-       require('../assets/mimikyu.png'),
-    ],
-    currentImage: 0,
-    shouldUpdateImage: false
+export default {
+  name: 'Members',
+  data() {
+    return {
+      images: [
+        require('../assets/mimikyu.png'),
+        require('../assets/mimikyu.png'),
+        require('../assets/mimikyu.png'),
+      ],
+      currentImage: 0,
     };
-},
-
-methods: {
+  },
+  methods: {
     previousImage() {
       this.currentImage = this.currentImage === 0 ? this.images.length - 1 : this.currentImage - 1;
-      this.shouldUpdateImage = true;
     },
     nextImage() {
       this.currentImage = this.currentImage === this.images.length - 1 ? 0 : this.currentImage + 1;
-      this.shouldUpdateImage = true;
     },
   },
+};
 
-}
 </script>
